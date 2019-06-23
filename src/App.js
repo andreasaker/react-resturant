eef7c7d95c4c0e090fca806a0fce980acaa7abdc
unknown <andreas.aker91@gmail.com>
2019-06-23 16:37:04 +0200

    git init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { Button, Header, Icon, Menu, Segment, Sidebar } from 'semantic-ui-react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [visible, setVisible] = useState(false)
+
+  let handleHideClick = () => setVisible(false);
+  let handleShowClick = () => setVisible(true);
+  let handleSidebarHide = () => setVisible(false);
+
+  let SidebarButton = props => {
+    if(props.visible){
+        return(
+        <button class="sidebarBtn" onClick={handleHideClick}>
+        <i class="icon close"></i>
+        </button>);
+      } else {
+        return(
+        <button class="sidebarBtn" onClick={handleShowClick}>
+          <i class="icon bars"></i>
+        </button>);
+      }
+  }
+
+    return (
+        <div>
+        <Sidebar.Pushable style={{height: '100vh'}} >
+          <Sidebar
+            as={Menu}
+            animation='push'
+            icon='labeled'
+            inverted
+            onHide={handleSidebarHide}
+            vertical
+            visible={visible}
+            width='thin'
+          >
+            <Menu.Item as='a'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='list' />
+              Menu
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='user circle' />
+              Contact
+            </Menu.Item>
+          </Sidebar>
+          
+          <Sidebar.Pusher>
+            <Segment basic>
+              <SidebarButton visible={visible}/>
+            
+              <Header as='h3'>Application Content</Header>
+              
+            </Segment>
+          </Sidebar.Pusher>
+          </Sidebar.Pushable>
+          </div> 
+         
+    )
+  
 }
+
+
 
 export default App;
